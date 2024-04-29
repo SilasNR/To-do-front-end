6c55e52a974792da39c867fff88561e9c199f45d
Silas Rodrigues Nascimento <silasnas2018@gmail.com>
2024-04-29 06:07:13 -0300

    atualização tela Login

--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import 'bootstrap/dist/css/bootstrap.css';
+import { Button, Card, Input, Label } from "reactstrap";
 
 
 
@@ -14,20 +16,60 @@ class App extends React.Component {
 
   }
 
-  
+
   render() {
-    
+
     return (
       <div>
-        <div style={{border:" 1px solid black", paddingLeft:"45%"}}> 
-        <form>
-          <h1>Login</h1>
-          <label >nome</label>
-          <input type="text" /><br/>
-          <label>senha</label> 
-          <input type="text" />
-        </form>
-        </div>
+        <Card className="w-25 p-2 mt-5 container">
+          <form>
+            <div className="row justify-content-center">
+              
+              <div className="col-12">
+                <h1>Login</h1>
+              </div>
+            </div>
+
+            <div className="row justify-content-center">
+              <div className="col-12">
+                <Label >Nome</Label>
+              </div>
+
+            </div>
+            <div className="row justify-content-center">
+              <div className="col-12">
+                <Input type="text" /><br />
+              </div>
+              
+            </div>
+            <div className="row justify-content-center">
+              <div className="col-12">
+                <Label >Senha</Label>
+              </div>
+              
+            </div>
+            <div className="row justify-content-center">
+              <div className="col-12">
+                <Input type="text" />
+              </div>
+              
+            </div>
+            <div className="row justify-content-center mt-4">
+              <div className="col-12">
+                <Button >Entrar</Button>
+              </div>
+              
+            </div>
+
+            <div className="row justify-content-center mt-4">
+              
+              <div className="col-12">
+                <h6><a>Esqueci minha senha</a></h6>
+              </div>
+            </div>
+
+          </form>
+        </Card>
       </div>
     );
   }
