40221678cf0b1a275b3924030e411dbf1dcfd20a
Silas Rodrigues Nascimento <silasnas2018@gmail.com>
2024-06-16 22:45:48 -0300

    teste

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -10,7 +10,7 @@ export default class Home extends React.Component {
             <div>
                 <div class="fundo-home">
 
-                    <Head estilo="home"/>
+                    <Head />
 
                     <main className="container d-flex flex-column align-items-center justify-content-center vh-100">
                         <h2 className="mb-4">Bem-vindo!</h2>
